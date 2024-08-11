9736899371620d8fddb6b5affa9a5647fd29225f
Shubham  Saurav <shubhamsinha186@gmail.com>
2024-08-11 21:01:57 +0530

    session 2: added category model and get and post operation on category

---
+++ index.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const connectDB = require('./db')
+
+
+const Category = require('./models/Category')
+
+
+connectDB()
+
+const app = express()
+
+// allows our app to read the body of the request
+app.use(express.json())
+
+
+app.post('/categories', async (req, res) => {
+    try {
+        const cat = await Category.create(req.body)
+        res.status(201).json({
+            success: true,
+            category: cat
+        })
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong",
+            error: error
+        })
+    }    
+})
+
+
+app.get('/categories', async(req, res) => {
+
+    try {
+        const categories = await Category.find()
+        res.status(200).json({
+            success: true,
+            count: categories.length,
+            categories
+        })
+
+    } catch (error) {
+        res.status(400).json({
+            success: false,
+            message: "Something went wrong",
+            error: error
+        })
+    }
+
+    
+})
+
+
+
+
+
+
+
+
+const PORT = 4000
+app.listen(PORT, () => {
+    console.log("Listening to server on port 4000");  
+})
